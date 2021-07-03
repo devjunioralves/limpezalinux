e207c497d80eaefc794e3d94e5f1791aaa830ee2
Junior Alves <junior@boxti.com.br>
2021-07-03 12:04:44 -0300

    Enviado script de limpeza

---
+++ limpeza.sh
@@ -0,0 +1,36 @@
+
+#!/bin/bash
+
+# Limpeza de arquivos inúteis no sistema
+# Por Junior Alves
+
+echo "Limpando a Lixeira"
+sudo rm -rf /home/$USER/.local/share/Trash/files/*
+echo ""
+echo ""
+echo "Limpando o diretório TMP"
+sudo rm -rf /var/tmp/*
+echo ""
+echo ""
+echo "Exclusão de cash inúteis do sistema. Cópias de atualizações do gerenciador de atualizações"
+sudo apt-get clean -y
+echo ""
+echo ""
+echo "Exclusão de programas que não estão sendo utilizados pelo sistema"
+sudo apt-get autoremove -y
+echo ""
+echo ""
+echo "Exclusão de arquivos duplicados"
+sudo apt-get autoclean -y
+echo ""
+echo ""
+echo "Reparando pacotes quebrados durante atualização"
+sudo dpkg --configure -a
+echo ""
+echo ""
+clear
+echo "Limpeza concluída"
+sleep 4
+exit
+
+
